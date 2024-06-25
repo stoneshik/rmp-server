31af34a5802dcf1a999512cbc85a653778588a19
stoneshik <strebilly@mail.ru>
2024-06-25 20:41:47 +0300

    Сигнализация + добавил логгирование запросов

--- src/main/kotlin/smart/server/controllers/SignalingDataController.kt
+++ src/main/kotlin/smart/server/controllers/SignalingDataController.kt
@@ -3,17 +3,43 @@ package smart.server.controllers
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 import smart.server.models.SignalingData
 
 @RestController
 class SignalingDataController {
-    private val signalingWorkingState = "work"
-    private val signalingState = true
+    private var signalingWorkingState = "work"
+    private var signalingState = true
+    private var counter = 0
+    private var SLEEPING_MILISECONDS = 5000L
 
     @GetMapping("/signaling-data")
     fun getFunctionTemperatureData(): ResponseEntity<SignalingData> {
+        counter++
+        if (counter > 30) {
+            signalingState = false
+        }
+        if (counter > 40) {
+            counter = 0
+            signalingState = true
+        }
         val signalingData = SignalingData(signalingWorkingState, signalingState)
         return ResponseEntity(signalingData, HttpStatus.OK)
+    }
+
+    @PostMapping("/signaling-data")
+    fun postFunctionTemperatureData() {
+        if (signalingWorkingState.equals("work")) {
+            signalingWorkingState = "turns_off"
+        } else if (signalingWorkingState.equals("switch_off")) {
+            signalingWorkingState = "turns_on"
+        }
+        Thread.sleep(SLEEPING_MILISECONDS)
+        if (signalingWorkingState.equals("turns_off")) {
+            signalingWorkingState = "switch_off"
+        } else if (signalingWorkingState.equals("turns_on")) {
+            signalingWorkingState = "work"
+        }
     }
 }
--- src/main/kotlin/smart/server/models/SignalingData.kt
+++ src/main/kotlin/smart/server/models/SignalingData.kt
@@ -1,3 +1,5 @@
 package smart.server.models
 
 data class SignalingData(val signalingWorkingState: String, val signalingState: Boolean)
+
+data class SignalingStateData(val signalingState: Boolean)
